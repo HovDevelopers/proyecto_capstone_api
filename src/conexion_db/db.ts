562e111b9134310d3118937cd4819bbc9a650fab
Markioni <73206031+JMarchioni@users.noreply.github.com>
2024-07-22 01:39:26 -0400

    db_credenciales

--- src/conexion_db/db.ts
+++ src/conexion_db/db.ts
@@ -3,11 +3,11 @@ import { join } from 'path';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
+  host: '10.67.119.186',
   port: 3306,
   username: 'root',
-  password: 'root',
-  database: 'sistemahospital',
+  password: 'Cb7A506a',
+  database: 'higia',
   entities: [join(__dirname, '/../**/*.entity{.ts,.js}')],
   synchronize: false,
 };
